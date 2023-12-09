078d0e408cff629a1306b863699e092bed180446
student <student@BTU.local>
2023-12-09 14:27:06 +0400

    Fix small error

--- Blog/resources/views/components/footer/footer.blade.php
+++ Blog/resources/views/components/footer/footer.blade.php
@@ -4,10 +4,10 @@
         <div class="row">
             <div class="col-md-6 footer-menu">footer-menu</div>
             <div class="col-md-6 contact-info">
-                {{ !empty(footer->email) ? footer->email : "test@gmail.com" }}
-                {{ !empty(footer->address) ? footer->address : "test address" }}
-                {{ !empty(footer->phone) ? footer->phone : "999 999 999" }}
-                {{ !empty(footer->about_us) ? footer->about_us : "test about us" }}
+                {{ !empty($footer->email) ? $footer->email : "test@gmail.com" }}
+                {{ !empty($footer->address) ? $footer->address : "test address" }}
+                {{ !empty($footer->phone) ? $footer->phone : "999 999 999" }}
+                {{ !empty($footer->about_us) ? $footer->about_us : "test about us" }}
             </div>
             <div class="col-md-12">
                 <div class="text-center">
